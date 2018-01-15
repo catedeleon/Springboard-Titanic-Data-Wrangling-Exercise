ceb8127bd0ab3e07e4552d8d27cf3588ee91743f
catedeleon <34855286+catedeleon@users.noreply.github.com>
2018-01-15 16:18:43 +0800

    Add files via upload

---
+++ Titanic_DR.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(tidyr)
+
+titanic_original <- read.csv("titanic_original.csv", na.strings = c("", "NA"))
+
+# Replace empty "embarked" values with "S"
+
+titanic_original$embarked[is.na(titanic_original$embarked)] <- "S"
+
+# Replace empty "age" values with age mean
+
+titanic_original$age[is.na(titanic_original$age)] <- mean(titanic_original$age, na.rm = TRUE)
+
+# Fill empty "boat" values with "None"
+
+titanic_original$boat <- as.character(titanic_original$boat)
+titanic_original$boat[is.na(titanic_original$boat)]<- "None"
+
+# Passengers w/ and w/o cabin
+
+titanic_original$has_cabin_number <- ifelse(is.na(titanic_original$cabin),"0","1")
+
+# Write csv file
+
+write.csv(titanic_original, file = "titanic_clean.csv")
